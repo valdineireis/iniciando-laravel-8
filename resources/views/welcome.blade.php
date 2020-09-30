3521d9bea52f8f525ea83df92c03564371325c75
thivalls <olavalls@gmail.com>
2020-09-30 18:55:25 -0300

    Aula 5 - Variáveis de Ambiente

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -45,6 +45,11 @@
                     </svg>
                 </div>
 
+                <?php
+                    echo env('MEU_NOME', 'Nome Default');
+                    // echo getenv('MEU_NOME');
+                ?>
+
                 <div class="mt-8 bg-white dark:bg-gray-800 overflow-hidden shadow sm:rounded-lg">
                     <div class="grid grid-cols-1 md:grid-cols-2">
                         <div class="p-6">
