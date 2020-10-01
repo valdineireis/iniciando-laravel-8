a2528e2039a72c5b7e620585798d76b2eeef8285
thivalls <olavalls@gmail.com>
2020-10-01 15:24:24 -0300

    Criando scaffolding bootstrap e falando sobre rotas

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,28 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Route::get('/hello', function(){
+//     // echo 'Hello School Of Net';
+//     return 'Hello School Of Net';
+// });
+
+// Route::get('/ola/{name}/{lastname?}', function($name, $lastname = ''){
+//     return "Hello {$name} {$lastname}";
+// });
+
+// Route::middleware('auth')->group(function(){
+
+//     Route::middleware('auth')->get('auth', function(){
+//         return 'rota 1';
+//     });
+
+//     Route::middleware('auth')->get('auth-2', function(){
+//         return 'rota 2';
+//     });
+
+// });
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
